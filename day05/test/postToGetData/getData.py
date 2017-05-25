55fb75244e8ec33c2e1325b6a2aed59e55aa3fa7
FlyPig <752325614@qq.com>
2017-05-25 14:11:42 +0800

    change02

---
+++ day05/test/postToGetData/getData.py
@@ -0,0 +1,74 @@
+#!usr/bin/env python3.6  
+#-*- coding:utf-8 -*-  
+""" 
+@author:iBoy 
+@file: getData.py 
+@time: 2017/05/24 
+"""
+import requests
+import re
+from lxml import etree
+import time
+from mongodb_queue import MongoQueue
+import multiprocessing
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:53.0) Gecko/20100101 Firefox/53.0'
+}
+
+url = 'http://www.sonin.mn/content/page'
+spider_queue = MongoQueue('mongolia', 'news02')
+
+
+def infoCrawler():
+    while True:
+        try:
+            page_id = spider_queue.pop()
+            print(page_id)
+        except KeyError:
+            print('队列咩有数据')
+            break
+        else:
+            getDataF1(page_id)
+            spider_queue.complete(page_id)
+
+
+def getDataF1(page_id):
+    data = {
+        'page_id':page_id, #"1",
+        'catgegory_id':"9",
+        'slug':"easy-page"
+    }
+
+    response = requests.post(url, data=data)
+
+    selector = etree.HTML(response.text)
+
+    all_titles = selector.xpath('//a[@class="title"]')
+    all_href = selector.xpath('//a[@class="title"]/@href')
+
+    for i in range(len(all_titles)):
+        title = all_titles[i].xpath('string(.)')
+        href = all_href[i]
+        href = 'http://www.sonin.mn' + href
+        print(title + ', ' + href)
+        with open('easy_page_urls.txt', 'a') as f:
+            f.write(href + '\n')
+
+def process_crawler():
+    process= []
+    # num_cpus = multiprocessing.cpu_count()
+    # print('将启动进程数为: ', num_cpus)
+    for i in range(30):
+        p = multiprocessing.Process(target=infoCrawler)
+        p.start()
+        process.append(p)
+    for p in process:
+        p.join()
+
+if __name__ == '__main__':
+    process_crawler()
+    # for i in range(56):
+    #     getDataF1(i)
+    #     time.sleep(1)
---
+++ day05/test/postToGetData/writePage_idToDB.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3.6  
+#-*- coding:utf-8 -*-  
+""" 
+@author:iBoy 
+@file: writePage_idToDB.py 
+@time: 2017/05/25 
+"""
+from mongodb_queue import MongoQueue
+
+spider_queue = MongoQueue('mongolia', 'news02')
+
+for i in range(546):
+    spider_queue.push(i)
+
+# with open('urls.txt') as f:
+#     data = f.readline()
+#     while data:
+#         spider_queue.push(data)
+#         print(data)
+#         data = f.readline()
